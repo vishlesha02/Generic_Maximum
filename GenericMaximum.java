fd8decf11cb821e2a9bc41713ddab64675d46228
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-05-01 23:36:27 +0530

    Add files via upload

--- GenericMaximum.java
+++ GenericMaximum.java
@@ -2,36 +2,34 @@ package Day9;
 import java.util.Scanner;
 public class GenericMaximum {
     
-    public void compare(){
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number 1 : ");
-        int num1 = sc.nextInt();
+    public void compare() {
 
-        System.out.println("Enter Number 2 : ");
-        int num2 = sc.nextInt();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Number 1 : ");
+            float num1 = sc.nextFloat();
 
-        System.out.println("Enter Number 3 : ");
-        int num3 = sc.nextInt();
+            System.out.println("Enter Number 2 : ");
+            float num2 = sc.nextFloat();
 
-        int maxNum = 0;
-        if ( num1 > num2 && num1 > num3 ) {
-             maxNum = num1;
+            System.out.println("Enter Number 3 : ");
+            float num3 = sc.nextFloat();
+
+            float maxNum = 0.0F;
+            if (num1 > num2 && num1 > num3) {
+                maxNum = num1;
+            } else if (num2 > num1 && num2 > num3) {
+                maxNum = num2;
+            } else if (num3 > num1 && num3 > num2) {
+                maxNum = num3;
+            }
+            System.out.println("The maximum number = " + maxNum);
         }
-        else if(num2> num1 && num2 > num3){
-            maxNum=num2;
+
+        public static class Maximum {
+            public static void main(String[] args) {
+
+                GenericMaximum  t = new  GenericMaximum ();
+                t.compare();
+            }
         }
-        else if(num3 > num1 && num3 > num2)
-        {
-            maxNum=num3;
-        }
-        System.out.println("The maximum number is = " + maxNum);
     }
-
-public static class Maximum {
-    public static void main(String[] args) {
-        
-        GenericMaximum t = new GenericMaximum();
-        t.compare();
-    }
-}}
